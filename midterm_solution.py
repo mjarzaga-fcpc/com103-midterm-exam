343474f253c414b04ee9c33b4d1cce2312c7bbf2
mjarzaga-fcpc <your_email@example.com>
2026-04-19 00:11:09 +0800

    after setting up variables2

--- midterm_solution.py
+++ midterm_solution.py
@@ -1,45 +1,13 @@
 student_name = str(input("Enter your Name: "))
-weekly_budget = int(input("Enter weekly budget: "))
+weekly_budget = int(input("Enter weekly budget: ")) 
 
 list_expenses = [
-    ["1.Food & Drinks:","Lunch, snacks, coffee"],
-    ["2.Transportaton:", "Bus, jeepney, ride-share"],
-    ["3.Mobile/Internet:", "Load, data plan, Wifi top-up"],
-    ["4.School Supplies:", "Notebook, pen, bond paper"],
-    ["5.Entertainment:", "Games, movies, hangout"],
+    [1,"Food & Drinks: Lunch, snacks, coffee"], 
+    [2,"Transportaton: Bus, jeepney, ride-share"],
+    [3,"Mobile/Internet: Load, data plan, Wifi top-up"],
+    [4,"School Supplies: Notebook, pen, bond paper"],
+    [5,"Entertainment: Games, movies, hangout"],
 
 ]
-for row in range(len(list_expenses)):
-    for col in range(len(list_expenses[row])):
-        print(list_expenses[row][col], end=" ")
-        print()
 
-       
-# for row in list_expenses:
-#     for element in row:
-#         print(element, end=" ")
-#         print()
-choice = int(input("Choose number 1 - 5: "))       
-total = 0
-while True:
-    total +=1
-    if choice == 1:
-            print("Lunch at the cafeteria")
-            continue
-    elif choice == 2:
-            print("Transportation")
-            continue
-    elif choice == 3:
-            print("Mobile Plan")
-            continue
-    elif choice == 4:
-            print("School Supplies")
-            continue
-    elif choice == 5:
-            print("Entertainment")
-            continue
-    elif choice == 0:
-            print("0 to skip")
-            break
-    else:
-        print("Invalid choice")
+list_budget = []
