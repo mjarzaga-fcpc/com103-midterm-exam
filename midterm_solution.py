bc6dde8e48aa1e50af5bef4f162141717c1f5b0b
mjarzaga-fcpc <your_email@example.com>
2026-04-19 00:11:49 +0800

    after setting up loops

--- midterm_solution.py
+++ midterm_solution.py
@@ -11,3 +11,26 @@ list_expenses = [
 ]
 
 list_budget = []
+
+# loops
+for row in list_expenses:
+    for element in row:
+        print(element, end=" ")
+        print()
+      
+for i in range(4):
+    print("Order", i + 1)
+
+    choice = int(input("Choose number (0 is skip) 0-5: "))
+ 
+    if choice >= 1:
+        print(choice)
+        description = str(input("Description: "))
+        amount = int(input("Amount: "))
+        if amount >= weekly_budget * .25:
+             list_budget.append([choice, description, True, amount])
+             continue
+        list_budget.append([choice, description, False,amount])
+    elif choice == 0:
+        print("Skipped")
+        break
